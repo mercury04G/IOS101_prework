879801adc391ef8bb78011a352c3615fc744b358
YingyGong <ygong@caltech.edu>
2023-08-27 22:48:12 -0700

    Initial Commit

---
+++ IOS101_prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS101_prework
+//
+//  Created by yingying gong on 8/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
